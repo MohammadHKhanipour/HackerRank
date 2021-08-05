fff4873270600421b74261e7c6a020183158a86f
Mohammad Hassan Khanipour <mohammadhkhanipour@outlook.com>
2021-08-05 19:48:20 +0430

    Added ServiceLane

---
+++ HackerRank/Challenges/ServiceLane.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+
+namespace HackerRank.Challenges
+{
+    class ServiceLane
+    {
+        public static int[] GetResult(int[] width, int[,] cases, int casesCount)
+        {
+            int[] result = new int[casesCount];
+
+            for (int i = 0; i < casesCount; i++)
+            {
+                result[i] = width.Skip(cases[i, 0]).Take(cases[i, 1] - cases[i, 0] + 1).Min();
+            }
+
+            return result;
+        }
+
+        public static void Run()
+        {
+            string[] inputs = Console.ReadLine().Split(' ');
+            int inputsCount = Convert.ToInt32(inputs[0]);
+            int casesCount = Convert.ToInt32(inputs[1]);
+
+            int[] width = new int[inputsCount];
+
+            inputs = Console.ReadLine().Split(' ');
+
+            width = Array.ConvertAll(inputs, int.Parse);
+
+            int[,] cases = new int[casesCount, 2];
+
+            string[] temp;
+
+            for (int i = 0; i < casesCount; i++)
+            {
+                temp = Console.ReadLine().Split(' ');
+                cases[i, 0] = Convert.ToInt32(temp[0]);
+                cases[i, 1] = Convert.ToInt32(temp[1]);
+            }
+
+            var result = GetResult(width, cases, casesCount);
+
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
--- HackerRank/Program.cs
+++ HackerRank/Program.cs
@@ -1,3 +1,3 @@
 ﻿using HackerRank.Challenges;
 
-BirthdayCakeCandles.Run();
+ServiceLane.Run();
