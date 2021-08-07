7dcf55b05a6a0370c80cc7a8e6b01df1b2334174
Mohammad Hassan Khanipour <mohammadhkhanipour@outlook.com>
2021-08-07 15:56:41 +0430

    Added NumberLineJumps

---
+++ HackerRank/Challenges/NumberLineJumps.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace HackerRank.Challenges
+{
+    class NumberLineJumps
+    {
+        public static string GetResult(int firstKangarooX, int firstKangarooV , int secondKangarooX, int secondKangarooV)
+        {
+            if (firstKangarooV > secondKangarooV)
+                if ((secondKangarooX - firstKangarooX) % (secondKangarooV - firstKangarooV) == 0)
+                    return "YES";
+            return "NO";
+        }
+
+        public static void Run()
+        {
+            int[] inputs = Console.ReadLine().Split(' ').Select(x => Convert.ToInt32(x)).ToArray();
+            Console.WriteLine(GetResult(inputs[0], inputs[1], inputs[2], inputs[3]));
+        }
+    }
+}
