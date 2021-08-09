293fb6a8cd5b5ad72a86f30e486de50c5026e65f
Mohammad Hassan Khanipour <mohammadhkhanipour@outlook.com>
2021-08-09 10:03:23 +0430

    Added BeautifulDaysAtTheMovies

---
+++ HackerRank/Challenges/BeautifulDaysAtTheMovies.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace HackerRank.Challenges
+{
+    class BeautifulDaysAtTheMovies
+    {
+        static int GetResult(int startScope, int endScope, int number)
+        {
+            int count = 0;
+            int reversedNumber;
+            for (int i = startScope; i <= endScope; i++)
+            {
+                reversedNumber = i.ToString().Reverse().Aggregate(0, (b, x) => 10 * b + x - '0');
+
+                if (Math.Abs(i - reversedNumber) % number == 0)
+                    count++;
+            }
+            return count;
+        }
+
+        public static void Run()
+        {
+            int[] inputs = Console.ReadLine().Split(' ').Select(x => Convert.ToInt32(x)).ToArray();
+            Console.WriteLine(GetResult(inputs[0], inputs[1], inputs[2]));
+        }
+    }
+}
