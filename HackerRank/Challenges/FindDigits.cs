ba77306588623d1735a4966fdc9bb220c77c8c56
Mohammad Hassan Khanipour <mohammadhkhanipour@outlook.com>
2021-08-13 19:12:19 +0430

    Added FindDigits

---
+++ HackerRank/Challenges/FindDigits.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace HackerRank.Challenges
+{
+    class FindDigits
+    {
+        static int GetResult(int number)
+        {
+            int divisors = 0;
+
+            string numberString = number.ToString();
+
+            foreach (char item in numberString)
+            {
+                if (Convert.ToInt32(item.ToString()) == 0)
+                    continue;
+                else if (number % Convert.ToInt32(item.ToString()) == 0)
+                    divisors++;
+            }
+
+            return divisors;
+        }
+
+        public static void Run()
+        {
+            int times = Convert.ToInt32(Console.ReadLine());
+
+            int[] results = new int[times];
+
+            for (int i = 0; i < times; i++)
+            {
+                results[i] = GetResult(Convert.ToInt32(Console.ReadLine()));
+            }
+
+            foreach (int item in results)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
