6ef29c816277f17b8a8c65a105e4ad98447c4958
Mohammad Hassan Khanipour <mohammadhkhanipour@outlook.com>
2021-08-15 18:59:41 +0430

    Added CountingValleys

---
+++ HackerRank/Challenges/CountingValleys.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HackerRank.Challenges
+{
+    class CountingValleys
+    {
+        static int GetResult(string route)
+        {
+            List<char> path = new List<char>();
+            path = route.ToCharArray().ToList();
+            int level = 0;
+            int valleyCounter = 0;
+            for (int i = 0; i < path.Count; i++)
+            {
+                switch (path[i])
+                {
+                    case 'U':
+                        level++;
+                        break;
+                    case 'D':
+                        level--;
+                        break;
+                }
+                if (level == 0 && path[i] == 'U')
+                {
+                    valleyCounter++;
+                }
+            }
+            return valleyCounter;
+        }
+
+        public static void Run()
+        {
+            _ = Console.ReadLine();
+
+            string route = Console.ReadLine();
+
+            Console.WriteLine(GetResult(route));
+        }
+    }
+}
--- HackerRank/Program.cs
+++ HackerRank/Program.cs
@@ -1,3 +1,3 @@
 ﻿using HackerRank.Challenges;
 
-SimpleArraySum.Run();
+CountingValleys.Run();
