d9712cb61c3b37a3a0825849056f178fcf674572
Mohammad Hassan Khanipour <mohammadhkhanipour@outlook.com>
2021-08-15 18:29:59 +0430

    Added SimpleArraySum

---
+++ HackerRank/Challenges/SimpleArraySum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace HackerRank.Challenges
+{
+    class SimpleArraySum
+    {
+        public static void Run()
+        {
+            int count = Convert.ToInt32(Console.ReadLine());
+
+            int[] array = new int[count];
+
+            array = Console.ReadLine().Split(' ').Select(x => Convert.ToInt32(x)).ToArray();
+
+            Console.WriteLine(array.Sum());
+        }
+    }
+}
--- HackerRank/Program.cs
+++ HackerRank/Program.cs
@@ -1,3 +1,3 @@
 ﻿using HackerRank.Challenges;
 
-SequenceEquation.Run();
+SimpleArraySum.Run();
