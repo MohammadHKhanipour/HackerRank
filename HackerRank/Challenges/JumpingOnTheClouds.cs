40f8cb0b770edbd74288f95127b13518de076a4d
Mohammad Hassan Khanipour <mohammadhkhanipour@outlook.com>
2021-08-17 19:58:46 +0430

    Added JumpingOnTheClouds

---
+++ HackerRank/Challenges/JumpingOnTheClouds.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace HackerRank.Challenges
+{
+    class JumpingOnTheClouds
+    {
+        static int GetResult(int[] input)
+        {
+            int counter = 0;
+            int i = 0;
+            while (i < (input.Length - 1))
+            {
+                if ((i + 2) != input.Length && input[i + 2] == 0)
+                {
+                    i += 2;
+                    counter += 1;
+                }
+                else
+                {
+                    i += 1;
+                    counter += 1;
+                }
+            }
+            return counter;
+        }
+
+        public static void Run()
+        {
+            int count = Convert.ToInt32(Console.ReadLine());
+
+            int[] array = new int[count];
+
+            array = Console.ReadLine().Split(' ').Select(x => Convert.ToInt32(x)).ToArray();
+
+            Console.WriteLine(GetResult(array));
+        }
+    }
+}
