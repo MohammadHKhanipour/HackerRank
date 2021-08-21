dfcc9946aff5ca1ac57a927e0a432a72327be45b
Mohammad Hassan Khanipour <mohammadhkhanipour@outlook.com>
2021-08-21 15:05:26 +0430

    Added MinMaxSum

---
+++ HackerRank/Challenges/MinMaxSum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace HackerRank.Challenges
+{
+    class MinMaxSum
+    {
+        static string GetResult(long[] numbers) => $"{numbers.Sum() - numbers.Max()} {numbers.Sum() - numbers.Min()}";
+
+        public static void Run()
+        {
+            long[] inputs = Console.ReadLine().Split(' ').Select(x => long.Parse(x)).ToArray();
+
+            Console.WriteLine(GetResult(inputs));
+        }
+    }
+}
