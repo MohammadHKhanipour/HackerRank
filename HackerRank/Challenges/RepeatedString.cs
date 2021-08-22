aa9bff64770aad66484d1120586e7a3746bd53e0
Mohammad Hassan Khanipour <mohammadhkhanipour@outlook.com>
2021-08-22 18:54:02 +0430

    Added RepeatedString

---
+++ HackerRank/Challenges/RepeatedString.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace HackerRank.Challenges
+{
+    class RepeatedString
+    {
+        static long GetResult(string inputString, long inputNum)
+        {
+            long counter = 0;
+            long div = inputNum / (inputString.Length);
+            int mod = Convert.ToInt32(inputNum % (inputString.Length));
+
+            counter = inputString.Count(x => x == 'a');
+
+            counter *= div;
+
+            counter += inputString.Take(mod).Count(x => x == 'a');
+
+            return counter;
+        }
+
+        public static void Run()
+        {
+            string inputString = Console.ReadLine();
+            long inputNum = long.Parse(Console.ReadLine());
+
+            Console.WriteLine(GetResult(inputString,inputNum));
+        }
+    }
+}
