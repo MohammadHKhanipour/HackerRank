ef9a44abe974535da72e8d58c9c4399b7fd2d17a
Mohammad Hassan Khanipour <mohammadhkhanipour@outlook.com>
2021-08-24 20:54:45 +0430

    Added TimeConversion

---
+++ HackerRank/Challenges/TimeConversion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HackerRank.Challenges
+{
+    class TimeConversion
+    {
+        static string GetResult(string input) => DateTime.Parse(input).ToString("HH:mm:ss");
+
+        public static void Run()
+        {
+            string input = Console.ReadLine();
+
+            Console.WriteLine(GetResult(input));
+        }
+    }
+}
--- HackerRank/Program.cs
+++ HackerRank/Program.cs
@@ -1,3 +1,3 @@
 ﻿using HackerRank.Challenges;
 
-AppleAndOrange.Run();
+TimeConversion.Run();
