7f7a167be495977de2de83079c6765d2f7ae6a99
Mohammad Hassan Khanipour <mohammadhkhanipour@outlook.com>
2021-08-28 20:08:50 +0430

    Added MarsExploration

---
+++ HackerRank/Challenges/MarsExploration.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace HackerRank.Challenges
+{
+    class MarsExploration
+    {
+        static int GetResult (string input)
+        {
+            int count = 0;
+            for (int i = 0; i < input.Length; i = i + 3)
+            {
+                if (input[i] != 'S')
+                    count++;
+                if (input[i + 1] != 'O')
+                    count++;
+                if (input[i + 2] != 'S')
+                    count++;
+            }
+
+            return count;
+        }
+
+        public static void Run()
+        {
+            Console.WriteLine(GetResult(Console.ReadLine()));
+        }
+    }
+}
--- HackerRank/Program.cs
+++ HackerRank/Program.cs
@@ -1,3 +1,3 @@
 ﻿using HackerRank.Challenges;
 
-TimeConversion.Run();
+MarsExploration.Run();
