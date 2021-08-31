26851ced076cb63cc33dfa9987c867b909bfd620
Mohammad Hassan Khanipour <mohammadhkhanipour@outlook.com>
2021-08-31 11:43:23 +0430

    Added SalesByMatch

---
+++ HackerRank/Challenges/SalesByMatch.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HackerRank.Challenges
+{
+    class SalesByMatch
+    {
+        static int GetResult(List<int> socks)
+        {
+            int pairCounter = 0;
+            List<int> tempList = new List<int>();
+
+            foreach (int item in socks)
+            {
+                if (tempList.Contains(item))
+                    continue;
+                pairCounter += socks.Where(x => x == item).Count() / 2;
+                tempList.Add(item);
+            }
+
+            return pairCounter;
+        }
+
+        public static void Run()
+        {
+            _ = Console.ReadLine();
+
+            List<int> inputs = Console.ReadLine().Split(' ').Select(x => Convert.ToInt32(x)).ToList();
+
+            Console.WriteLine(GetResult(inputs));
+        }
+    }
+}
