5f336fd1ec2fb60284870719c28b18a3cbc4b9b6
Mohammad Hassan Khanipour <mohammadhkhanipour@outlook.com>
2021-09-06 13:14:01 +0430

    Added ViralAdvertising

---
+++ HackerRank/Challenges/ViralAdvertising.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace HackerRank.Challenges
+{
+    class ViralAdvertising
+    {
+        static int GetResult(int dayNumber)
+        {
+            int temp = 2;
+            int a = 2;
+            for (int i = 1; i < dayNumber; i++)
+            {
+                a *= 3;
+                a /= 2;
+                temp += a;
+            }
+
+            return temp;
+        }
+
+        public static void Run()
+        {
+            int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(GetResult(dayNumber));
+        }
+    }
+}
--- HackerRank/Program.cs
+++ HackerRank/Program.cs
@@ -1,3 +1,3 @@
 ﻿using HackerRank.Challenges;
 
-MigratoryBirds.Run();
+ViralAdvertising.Run();
