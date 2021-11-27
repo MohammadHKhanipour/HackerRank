0f1080a1319d4918517b7ab06210a0d7acfe5bba
Mohammad Hassan Khanipour <mohammadhkhanipour@outlook.com>
2021-11-27 21:13:08 +0330

    Updated to .NET 6

--- HackerRank/Challenges/AVeryBigSum.cs
+++ HackerRank/Challenges/AVeryBigSum.cs
@@ -4,7 +4,6 @@ class AVeryBigSum
 {
     public static void Run()
     {
-        Console.WriteLine("TEST");
         _ = Console.ReadLine();
 
         long[] numbers = Console.ReadLine().Split(' ').Select(x => long.Parse(x)).ToArray();
